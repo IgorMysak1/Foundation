a439a564e289dda4b0c42cf647ad579dc3e574b2
Igor <igormusak1@gmail.com>
2021-01-24 23:01:47 +0200

    second edits

--- #src/js/custom.js
+++ #src/js/custom.js
@@ -15,19 +15,16 @@ arrowPopUp.addEventListener('click', function () {
 let emailInput = document.querySelector('.enter__email');
 let getStarted = document.querySelector('.content-email__btn');
 let noticeMistake = document.querySelector('.notice__mistake');
-
+let regEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 getStarted.addEventListener('click' ,checkEmail);
 
 function checkEmail(){
-	if(emailInput.value.indexOf("@") > -1){
-		emailInput.value = "";
+	if(emailInput.value.match(regEmail)){
 		if(document.querySelector('.enter__email.active')){
-			emailInput.classList.remove("active");
-			noticeMistake.classList.remove("active");
+			emailInput.classList.remove('active');
 		}
 	}else{
-		emailInput.classList.add("active");
-		emailInput.classList.add("active");
+		emailInput.classList.add('active');
 	}
 }
